1500d258c991646039971f34f2952a03b5037be5
txpmr <58395318+txpmr@users.noreply.github.com>
2025-08-03 11:02:30 -0500

    Create User.php

---
+++ app/Models/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'username',
+        'email',
+        'password',
+        'role',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
+
+    public function isPremium()
+    {
+        return in_array($this->role, ['premium', 'band', 'model']);
+    }
+}
